ffebeb8f271ea52e6c3a06c84c5db92d1a4f365b
RuanFailache <ruanfailache@outlook.com>
2021-08-05 12:06:03 -0300

    sending a message to the server

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
+const URL_SERVER = "https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages";
+
+let username;
+
 const people = document.querySelector(".people");
 const outside = document.querySelector(".outside-options");
-const sendMessage = document.querySelector(".send-message");
+const login = document.querySelector(".login");
 
 // Part 1 - Event Listeners
 
@@ -12,26 +16,78 @@ outside.addEventListener("click", () => {
   document.querySelector(".options-window").classList.add("hidden");
 });
 
-sendMessage.addEventListener("click", () => {
-  const messages = document.querySelector(".messages");
-  messages.innerHTML += `<li class="message normal">${document.querySelector(".new-message").value}</li>`;
+login.addEventListener("click", () => {
+  username = document.querySelector(".username").value;
+  
+  if (username) {
+    document.querySelector(".initial-page").classList.add("hidden");
+    document.querySelector(".chat").classList.remove("hidden");
+  } else {
+    alert("Por favor, digite um nome de usuario valido!");
+  }
+
+  loginServer(username);
+
 });
 
 // Part 2 - Management of messages with promisses
 
-function checkServer () {
-  const promise = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages");
+function loginServer (username) {
+  const time = new Date();
+
+  const message = {
+    from: username,
+    to: "Todos",
+    text: "entra na sala...",
+    type: "status",
+    time: time.toLocaleTimeString().slice(0, 8)
+  }
+
+  console.log(message);
+
+  const promise = axios.post(URL_SERVER, message);
+
   promise.then(checkMessages);
+  promise.catch(checkError);
+}
+
+function checkServer () {
+  const promise = axios.get(URL_SERVER);
+  promise.then(checkMessages);
+  promise.catch(checkError);
 }
 
 function checkMessages (response) {
   const messages = document.querySelector(".messages");
-  console.log(response.data.type);
-  for (let message of response.data)
-    if (message.type === "status")
-      messages.innerHTML += `<li class="${message.type}"><span class="time">(${message.time})</span> <strong>${message.from}</strong> ${message.text}</li>`;
-    else if (message.type === "message")
-      messages.innerHTML += `<li class="${message.type}"><span class="time">(${message.time})</span> <strong>${message.from}</strong> para <strong>${message.to}:</strong> ${message.text}</li>`;
+  messages.innerHTML = "";
+  for (let message of response.data) {
+    switch (message.type) {
+      case "status":
+        messages.innerHTML += `<li class="${message.type}"><span class="time">(${message.time})</span> <strong>${message.from}</strong> ${message.text}</li>`;
+        break;
+      
+      case "message":
+        messages.innerHTML += `<li class="${message.type}"><span class="time">(${message.time})</span> <strong>${message.from}</strong> para <strong>${message.to}:</strong> ${message.text}</li>`;
+        break;
+      
+      default:
+        console.log("mensagem desconhecida");
+        break;
+    }
+  }
 }
 
-checkServer();
+function checkError (error) {
+  switch (error.response.status) {
+    case 404:
+      break;
+    case 409:
+      break;
+    case 422:
+      break;
+    default:
+      break;
+  }
+}
+
+setInterval(checkServer, 3000);
